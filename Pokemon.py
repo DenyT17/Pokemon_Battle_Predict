6845bf409ddc1f205b47a36ec95fe09ee37bb999
DenyT17 <dan.topa@wp.pl>
2023-03-05 14:05:30 +0100

    pred for any two pokemon

--- Pokemon.py
+++ Pokemon.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from Analisis import analisis
+from Function import analisis, pokemon_battle
 from sklearn import (datasets, metrics,model_selection as skms,neighbors)
 from sklearn.tree import DecisionTreeClassifier
 
@@ -20,7 +20,7 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoost
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler
+
 
 # Read data from csv files
 pd.set_option('display.max_columns', None)
@@ -32,37 +32,6 @@ test_data=pd.read_csv('tests.csv')
 train, y_train = analisis(poke_data,combat_data)
 x_train = train.drop(
     ["Winner", 'Type 1', 'Type 2', 'Oponent Type 1', 'Oponent Type 2', 'Legendary', 'Oponent Legendary'], axis=1)
-# Creating combat data
-
-name1 = "Bulbasaur"
-name2 = "Ivysaur"
-
-pokemon1 = poke_data.loc[poke_data['Name'] == name1].reset_index()
-pokemon2 = poke_data.loc[poke_data['Name'] == name2].reset_index()
-pokemon1 = pokemon1.drop(['index','#','Name','Type 1','Type 2','Legendary'],axis=1)
-pokemon2 = pokemon2.drop(['index','#','Name','Type 1','Type 2','Legendary'],axis=1)
-pokemon2.rename(columns={ 'Type 1': 'Oponent Type 1', 'Type 2': 'Oponent Type 2',
-                               'HP': 'Oponent HP', 'Attack': 'Oponent Attack', 'Defense': 'Oponent Defense',
-                               'Sp. Atk': 'Oponent Sp. Atk', 'Sp. Def': 'Oponent Sp. Def', 'Speed': 'Oponent Speed',
-                               'Generation': 'Oponent Generation'
-                               }, inplace=True)
-
-pokemon_battle = pd.concat([pokemon1,pokemon2],axis=1)
-pokemon_battle = pd.concat([pokemon_battle,x_train],axis=0).reset_index()
-pokemon_battle = pokemon_battle.drop(['index'],axis=1)
-print(pokemon_battle)
-for column in pokemon_battle.columns:
-    ages_data = np.array(train[column]).reshape(-1, 1)
-    pokemon_battle[column] = StandardScaler().fit_transform(ages_data)
-
-
-
-
-
-
-
-
-
 
 # Winner prediction
 x_train = train.drop(
@@ -79,3 +48,13 @@ model =RandomForestClassifier()
 fit = model.fit(poke_train_ftrrs, poke_train_trg)
 preds = fit.predict(poke_test_ftrs)
 print("Fit: ", metrics.accuracy_score(poke_test_trg, preds))
+
+filename = "RandomForestClassifier.joblib"
+joblib.dump(model, filename)
+
+# Creating combat data
+pokemon_battle_input = pokemon_battle('Virizion','Terrakion',poke_data,x_train)
+model = joblib.load("RandomForestClassifier.joblib")
+
+proba = model.predict_proba(pokemon_battle_input)
+print(proba)
